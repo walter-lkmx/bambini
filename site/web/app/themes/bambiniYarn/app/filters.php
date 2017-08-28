3b38ad2edfcdbb926826d8a35def8cb6bf17afc1
walter hurtado <walter.hurtado@lkmx.io>
2017-08-27 17:52:11 -0700

    update

--- site/web/app/themes/bambiniYarn/app/filters.php
+++ site/web/app/themes/bambiniYarn/app/filters.php
@@ -69,7 +69,7 @@ add_filter('template_include', function ($template) {
  */
 add_filter('comments_template', 'App\\template_path');
 
-add_filter( 'query', 'fix_ajax_query');
+add_filter( 'query', __NAMESPACE__ . 'fix_ajax_query');
 function fix_ajax_query( $query ) {
 $pattern = '/^\s*(START TRANSACTION|COMMIT|ROLLBACK)/i';
 return preg_match( $pattern, $query ) ? '' : $query;
