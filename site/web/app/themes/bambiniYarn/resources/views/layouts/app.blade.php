42168e7c5c04c8dd430ed2d84a2bfeaef0f94bf3
walter hurtado <walter.hurtado@lkmx.io>
2017-11-16 15:01:24 -0800

    update google tags

--- site/web/app/themes/bambiniYarn/resources/views/layouts/app.blade.php
+++ site/web/app/themes/bambiniYarn/resources/views/layouts/app.blade.php
@@ -2,6 +2,10 @@
 <html @php(language_attributes())> 
   @include('partials.head')
   <body @php(body_class()) style="display: none;">
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M9JKVZS"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     @php(do_action('get_header'))
     @include('partials.header')
     <div class="wrap container" role="document">
