35a35173d667daa4bff6aa413f2098c6ce168af7
walter hurtado <walter.hurtado@lkmx.io>
2017-12-05 12:45:39 -0800

    update tawk.to script

--- site/web/app/themes/bambiniYarn/resources/views/partials/footer.blade.php
+++ site/web/app/themes/bambiniYarn/resources/views/partials/footer.blade.php
@@ -40,7 +40,7 @@
 </footer>
 
 <!--Start of Tawk.to Script-->
-<script type="text/javascript">
+{{-- <script type="text/javascript">
 var Tawk_API = Tawk_API || {},
     Tawk_LoadStart = new Date();
 (function() {
@@ -53,5 +53,29 @@ var Tawk_API = Tawk_API || {},
     s0.parentNode.insertBefore(s1, s0);
 })();
 
-</script>
+</script> --}}
 <!--End of Tawk.to Script-->
+
+
+<!--Adview Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/59515c6a50fd5105d0c82c92/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+    Tawk_API = Tawk_API || {};
+    Tawk_API.onOfflineSubmit = function(){
+      wc_event_ypbib
+      (
+        "form", 
+        "click", 
+        "chat" 
+      );
+    };
+</script>
+<!--Adview End of Tawk.to Script-->
