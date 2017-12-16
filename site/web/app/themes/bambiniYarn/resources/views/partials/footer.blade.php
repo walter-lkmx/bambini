b8b99d076772ca7cf7e27defeaf01c0e31f6b172
walter hurtado <walter.hurtado@lkmx.io>
2017-12-15 17:05:27 -0800

    update adview script

--- site/web/app/themes/bambiniYarn/resources/views/partials/footer.blade.php
+++ site/web/app/themes/bambiniYarn/resources/views/partials/footer.blade.php
@@ -58,6 +58,7 @@ var Tawk_API = Tawk_API || {},
 
 
 <!--Adview Start of Tawk.to Script-->
+<!--Start of Tawk.to Script-->
 <script type="text/javascript">
 var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
@@ -78,4 +79,5 @@ s0.parentNode.insertBefore(s1,s0);
       );
     };
 </script>
+<!--End of Tawk.to Script-->
 <!--Adview End of Tawk.to Script-->
