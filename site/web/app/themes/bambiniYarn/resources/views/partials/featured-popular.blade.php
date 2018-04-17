8b35d277a780c9db0022003ba0a056522dc4e2fd
edith moreno <edith@Dianas-Mac-mini.local>
2018-04-17 16:00:28 -0700

    I hope these changes help website’s bugs…

--- site/web/app/themes/bambiniYarn/resources/views/partials/featured-popular.blade.php
+++ site/web/app/themes/bambiniYarn/resources/views/partials/featured-popular.blade.php
@@ -1,21 +1,24 @@
+<div class="sidebar">
+  @include('partials.sidebar') 
+</div>
 <div class="featured-products">
     <h3>Lo más popular</h3>
     <?php
-          echo do_shortcode('[featured_products per_page="4"]');
+          echo do_shortcode('[featured_products per_page="6"]');
     ?>
 
 </div>
 <div class="featured-products">
     <h3>Te recomendamos</h3>
     <?php
-          echo do_shortcode('[product_tag tags="te-recomendamos" per_page="4"]');
+          echo do_shortcode('[product_tag tags="te-recomendamos" per_page="6"]');
     ?>
 
 </div> 
 <div class="featured-products">
     <h3>Tendencias</h3>
     <?php
-          echo do_shortcode('[product_tag tags="tendencias" per_page="4"]');
+          echo do_shortcode('[product_tag tags="tendencias" per_page="6"]');
     ?>
 
 </div>
