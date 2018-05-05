1bfb4d4475921664d0e8e14b04945600b6b32103
edith moreno <edith@Dianas-Mac-mini.local>
2018-05-04 17:06:38 -0700

    ….remaining style settings.

--- site/web/app/themes/bambiniYarn/resources/views/partials/search-box.blade.php
+++ site/web/app/themes/bambiniYarn/resources/views/partials/search-box.blade.php
@@ -1,10 +1,11 @@
 <div class="search-box">
-    <div class="container">
-        <!--img class="first-wave" src="@asset('images/wave-horizontal.svg')" alt=""-->
-        <h3>Producto del mes</h3>
-        <h4><span>Carriola</span></h4>
-        <h4>Graco Verb Click Connect</h4>    
-        <img src="@asset('images/producto-carriola.png')" alt="">    
-        <!--img class="second-wave" src="@asset('images/wave-horizontal.svg')" alt=""-->
-    </div>
+    	<div class="container">
+	        <!--img class="first-wave" src="@asset('images/wave-horizontal.svg')" alt=""-->
+	        <a href="https://bambini.club/producto/gracoverbclickconnectcochecitochilired/" target="_blank">
+	        <h3>Producto del mes</h3>
+	        <h4><span>Carriola</span></h4>
+	        <h4>Graco Verb Click Connect</h4>    
+	        <img src="@asset('images/producto-carriola.png')" alt=""></a>
+	        <!--img class="second-wave" src="@asset('images/wave-horizontal.svg')" alt=""-->
+    	</div>
 </div>
