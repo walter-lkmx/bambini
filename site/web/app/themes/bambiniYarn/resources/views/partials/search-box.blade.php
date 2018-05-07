ebff52ec0dbcec1f0046a7917172b0e11229a92c
edith moreno <edith@Dianas-Mac-mini.local>
2018-05-07 09:29:54 -0700

    Style settings: mobile responsiveness for slider.

--- site/web/app/themes/bambiniYarn/resources/views/partials/search-box.blade.php
+++ site/web/app/themes/bambiniYarn/resources/views/partials/search-box.blade.php
@@ -2,10 +2,13 @@
     	<div class="container">
 	        <!--img class="first-wave" src="@asset('images/wave-horizontal.svg')" alt=""-->
 	        <a href="https://bambini.club/producto/gracoverbclickconnectcochecitochilired/" target="_blank">
-	        <h3>Producto del mes</h3>
-	        <h4><span>Carriola</span></h4>
-	        <h4>Graco Verb Click Connect</h4>    
-	        <img src="@asset('images/producto-carriola.png')" alt=""></a>
+		        <h3>Producto del mes</h3>
+		        <h4><span>Carriola</span></h4>
+		        <h4>Graco Verb Click Connect</h4>
+		        <div class="product-image">    
+		        	<img src="@asset('images/producto-carriola.png')" alt="">
+		        </div>
+	        </a>
 	        <!--img class="second-wave" src="@asset('images/wave-horizontal.svg')" alt=""-->
     	</div>
 </div>
