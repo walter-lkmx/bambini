71d72407d57396a378c57a0afde4058f0c34b436
edith moreno <edith@Dianas-Mac-mini.local>
2018-05-08 08:33:07 -0700

    New images for slider.

--- site/web/app/themes/bambiniYarn/resources/views/partials/slider.blade.php
+++ site/web/app/themes/bambiniYarn/resources/views/partials/slider.blade.php
@@ -4,15 +4,19 @@
     <div class="promos"> 
         <ul>
             <li>
-                <img src="@asset('images/envios-gratis.png')" alt="bambini.club">
+                <img src="@asset('images/dia-madres.png')" alt="bambini.club">
                 {{-- <h3>Lorem ipsum</h3> --}}
             </li>
             <li>
-                <img src="@asset('images/bamb-s02.png')" alt="bambini.club">
+                <img src="@asset('images/carriolas-slider.png')" alt="bambini.club">
                 {{-- <h3>dolor sit amet</h3> --}}
             </li>
             <li>
-                <img src="@asset('images/bamb-s03.png')" alt="bambini.club">
+                <img src="@asset('images/envios-gratis.png')" alt="bambini.club">
+                {{-- <h3>consectetur adipiscing</h3> --}} 
+            </li>
+            <li>
+                <img src="@asset('images/horarios.png')" alt="bambini.club">
                 {{-- <h3>consectetur adipiscing</h3> --}} 
             </li>
         </ul>
