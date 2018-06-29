8da1b5658f33e002a2e2c58b11c2cdca3c208e6e
edith moreno <edith@Dianas-Mac-mini.local>
2018-06-29 12:42:33 -0700

    New “Product of the Month” + CSS settings.

--- site/web/app/themes/bambiniYarn/resources/views/partials/search-box.blade.php
+++ site/web/app/themes/bambiniYarn/resources/views/partials/search-box.blade.php
@@ -1,22 +1,14 @@
 <div class="search-box">
     	<div class="container">
 	        <!--img class="first-wave" src="@asset('images/wave-horizontal.svg')" alt=""-->
-	        <a href="https://bambini.club/producto/gracoverbclickconnectcochecitochilired/" target="_blank">
-		        <h3>Producto del mes</h3>
-		        <h4><span>Carriola</span></h4>
-		        <h4>Graco Verb Click Connect</h4>
-		        <div class="product-image">    
-		        	<img src="@asset('images/producto-carriola.png')" alt="">
-		        </div>
-	        </a>
-	        <!--a href="https://bambini.club/producto/diaperdudeconvertiblemochilapaalbolsacaramelo/" target="_blank">
+	        <a href="https://bambini.club/producto/diaperdudeconvertiblemochilapaalbolsacaramelo/" target="_blank">
 		        <h3>Producto del mes</h3>
 		        <h4><span>Pañalera</span></h4>
 		        <h4>Diaper Dude Convertible</h4>
 		        <div class="product-image">    
 		        	<img src="@asset('images/producto-panalera.png')" alt="">
 		        </div>
-	        </a-->
+	        </a>
 	        <!--img class="second-wave" src="@asset('images/wave-horizontal.svg')" alt=""-->
     	</div>
 </div>
