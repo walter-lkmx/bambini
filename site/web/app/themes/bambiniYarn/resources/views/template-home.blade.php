e7a0c15e19fd9232ff4f80b8543ab326df9118b5
edith moreno <edith@Dianas-Mac-mini.local>
2019-03-08 10:52:32 -0800

    Style restored on Home.

--- site/web/app/themes/bambiniYarn/resources/views/template-home.blade.php
+++ site/web/app/themes/bambiniYarn/resources/views/template-home.blade.php
@@ -7,16 +7,47 @@
 @section('content')
   @while(have_posts()) @php(the_post())
     @include('partials.page-header')
-    <div class="welcome welk0mesx">
+    <div class="welcome">
         <div class="container">
             @include('partials.slider')
             @include('partials.search-box')
         </div>
     </div>
-    {{-- @include('partials.featured-recommended') --}}
-    @include('partials.featured-popular')
-    {{-- @include('partials.featured-trending') --}}
-    @include('partials.content-page')
-  @endwhile
+    <div class="products-content">
+      {{-- @include('partials.featured-recommended') --}}
+      @include('partials.featured-popular')
+      {{-- @include('partials.featured-trending') --}}
+      @include('partials.content-page')
+      @endwhile
+      <?php 
+        $related_args = array(
+          'orderby' => 'rand',
+          'post_type' => 'post',
+          'posts_per_page' => 4,
+        );
+        $related = new WP_Query();
+        if( $related->have_posts() ) :
+      ?>
+      <div class="post-navigation">
+        <h3>Te recomendamos leer</h3>
+        <ul class="related">
+          <?php while( $related->have_posts() ): $related->the_post(); ?>
+          <li>
+            <div class="related-image"><a href="{{ get_permalink() }}">{{ the_post_thumbnail() }}</a></div>
+            <a href="{{ get_permalink() }}">{{ the_title() }}</a>
+          </li>
+          <?php endwhile; ?>
+        </ul>
+      </div>
+      <?php endif; ?>
+      <aside class="sidebar">
+          <a><img src="@asset('images/espacio1.png')"></a>
+          <a><img src="@asset('images/espacio2.png')"></a>
+          <a><img src="@asset('images/espacio3.png')"></a>
+          <a><img src="@asset('images/espacio4.png')"></a>
+          <a><img src="@asset('images/espacio5.png')"></a>
+          <a><img src="@asset('images/espacio6.png')"></a>
+      </aside>
+    </div>
   @include('partials.useful-info')
 @endsection
